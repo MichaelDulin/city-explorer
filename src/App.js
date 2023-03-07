efc32a36006285cdaabdb502f0d825a34c638ed5
MichaelDulin <michaeldulin1211@gmail.com>
2023-03-07 02:06:15 +0000

    added API but broke from npm install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Header from "./Header";
+import Footer from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "react-bootstrap/Form";
+import "./App.css";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      location: {},
+    };
+  }
 
+  getLocation = async () => {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+    const res = await axios.get(API);
+    console.log(res.data[0]);
+    this.setState({ location: res.data[0] });
+  };
+
+  render() {
     return (
       <>
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Form.group className="mb-5 border p-5">
+          <Form.Label>
+            Enter a location to find lattitude and longitude:{" "}
+          </Form.Label>
+          <Form.Control
+            placeholder="Enter a location..."
+            onChange={(e) => this.setState({ searchQuery: e.target.value })}
+          />
+          <button onClick={this.getLocation}>Explore!</button>
+          {this.state.location.place_id && (
+            <h2>The city is: {this.state.location.display_name}</h2>
+          )}
+        </Form.group>
+        <Footer />
       </>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+
+    return (
+      <>
+        <footer>copy& Michael Dulin</footer>
+      </>
+    );
+  }
+}
+
+export default Footer;
