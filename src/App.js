ace2faa781b740aa74833c90eadf4aad42be8aba
MichaelDulin <michaeldulin1211@gmail.com>
2023-03-09 00:49:24 +0000

    added hide feature / error handling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ class App extends React.Component {
       cityData: {},
       lat: "",
       long: "",
+      error: false,
+      errorMsg: "",
       reults: false,
+      weatherData: {},
     };
   }
 
@@ -27,40 +30,56 @@ class App extends React.Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
-    let getCityData = await axios.get(
-      `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`
-    );
+    try {
+      let getCityData = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`
+      );
 
-    this.setState({
-      results: true,
-      cityData: getCityData.data[0],
-      lat: getCityData.data[0].lat,
-      long: getCityData.data[0].lon,
-    });
+      this.setState({
+        results: true,
+        cityData: getCityData.data[0],
+        cityDataName: getCityData.data[0].display_name,
+        lat: getCityData.data[0].lat,
+        long: getCityData.data[0].lon,
+        error: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMsg: `ERROR: ${error.response.status}`
+      })
+    }
   };
+
 
   render() {
     let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.long}&zoom=11`;
     return (
       <>
         <Header />
+
         <CityForm
           className="mb-5 p-5 "
           handleSubmit={this.handleSubmit}
           handleInput={this.handleInput}
           cityData={this.state.cityData}
         />
-        {this.state.results
-          && (
-            <div className="results">
-              <ul className="">
-                <li>{this.state.cityName}</li>
-                <li>Latitude: {this.state.lat}</li>
-                <li>longitude: {this.state.long}</li>
-              </ul>
-              <img className="shadow bg body" src={mapURL} alt={this.state.cityName} ></img>
-            </div>
-          )};
+
+        {this.state.results && (
+          <div className="results">
+            <ul className="">
+              <li>{this.state.cityDataName}</li>
+              <li>Latitude: {this.state.lat}</li>
+              <li>longitude: {this.state.long}</li>
+            </ul>
+            <img
+              className="shadow bg body"
+              src={mapURL}
+              alt={this.state.cityDataName}
+            ></img>
+          </div>
+        )}
+
         <Footer />
       </>
     );
