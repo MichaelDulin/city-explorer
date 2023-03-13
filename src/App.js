f55382d11fff6a4cdf84f6f80c8f505d94610d5a
MichaelDulin <michaeldulin1211@gmail.com>
2023-03-13 20:45:52 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -59,9 +59,7 @@ class App extends React.Component {
 
   handleWeather = async () => {
     this.handleMovies();
-    let weatherUrl = await axios.get(
-      `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.lat}&lon=${this.state.long}`
-    );
+    let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.lat}&lon=${this.state.long}`;
     let getWeather;
     try{
       getWeather = await axios.get(weatherUrl);
@@ -78,9 +76,7 @@ class App extends React.Component {
   }
 
   handleMovies = async () => {
-    let movieUrl = await axios.get(
-      `${process.env.REACT_APP_SERVER}/movie?city=${this.state.cityName}`
-    );
+    let movieUrl = `${process.env.REACT_APP_SERVER}/movie?city=${this.state.cityName}`;
     let getMovies;
     try{
       getMovies = await axios.get(movieUrl);
@@ -98,15 +94,16 @@ class App extends React.Component {
 
   render() {
     let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.long}&zoom=11`;
-    let forecast = this.state.weatherData.map((weather) => {
-      return <Weather date={weather.date} description={weather.description} />;
+    let forecast = this.state.weatherData.map((weather, index) => {
+      return <Weather key={index} date={weather.date} description={weather.description} />;
     });
 
     let movies = this.state.movieData.map((movie) => {
       return (
         <Movie
+        key={movie.title}
           title={movie.title}
-          img_url={movie.img_url}
+          img_url={movie.image_url}
           released_on={movie.released_on}
         />
       );
